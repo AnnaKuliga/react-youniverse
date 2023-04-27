bc9d18529a9d91012e6772f0e953c0029a398e47
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-04-27 13:47:00 +0200

    started react app and added plans/programs data

---
+++ assets/programsData.js
@@ -0,0 +1,17 @@
+export const programsData = [
+  {
+    heading: "Przygotowanie do egzaminu 8 klasisty",
+    details:
+      "Na zajęciach zajmiemy się powtórzeniem materiału do matury. Powtórzymy zagadnienia gramatyczne, słownitwo, formy pisemne oraz skupimy się na konwersacjach. ",
+  },
+  {
+    heading: "Przygotowanie do matury",
+    details:
+      "Na zajęciach zajmiemy się powtórzeniem materiału do matury. Powtórzymy zagadnienia gramatyczne, słownitwo, formy pisemne oraz skupimy się na konwersacjach. ",
+  },
+  {
+    heading: "Zajęcia powtórzeniowe",
+    details:
+      "Na zajęciach zajmiemy się opanowaniem bieżącego materiału oraz rozszerzeniem zagadnień. Dodatkowo, zajmiemy sie przygotowanień do egzaminów, sprawdzianów i kartkówek. Będziemy ćwiczyć konwersacje i praktycznego użycie języka angielskiego, tak aby uczeń nie bał się rozmawiać po angielsku. ",
+  },
+];
